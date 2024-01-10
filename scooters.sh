ec3c433fe77cebd858b318e5c525424371839691
Adam Esbjörnsson <ades22@student.bth.se>
2024-01-10 09:01:07 +0100

    bash scripts for startup

--- scooters.sh
+++ scooters.sh
@@ -1,8 +1,29 @@
+#!/bin/bash
+
+if [ $1 == "stop" ]; then
+    docker stop $(docker ps -a | grep bestscooter.azurecr.io/scooter-app | awk '{print $1}')
+    exit 0
+fi
+
 counter=1
-while [ $counter -le 1000 ]
-do
-  docker run -d -e SCOOTERID=$counter -e PASSWORD=$counter --network docker_bestscooter-network --link api-server:bestscooter.azurecr.io/api-server --link ws-server:bestscooter.azurecr.io/ws-server --volume hardware:/scooter-app/model/hardware/ bestscooter.azurecr.io/scooter-app
-  ((counter++))
+while [ $counter -le $(($1)) ]; do
+    docker run -d \
+    -e SCOOTER_ID=$counter \
+    -e HARDWARE_PATH=/scooter-app/model/hardware/ \
+    -e LOG_PATH=scooter-trips.log \
+    -e NODE_ENV=simulation \
+    -e BACKEND=http://api-server:1337/ \
+    -e VERSION=v1 \
+    -e HARDWARE_UPDATE=5000 \
+    -e WS_URL=ws://ws-server:8081/ \
+    -e TOKEN="" \
+    --network docker_bestscooter-network \
+    --link api-server:bestscooter.azurecr.io/api-server \
+    --link ws-server:bestscooter.azurecr.io/ws-server \
+    --volume docker_hardware:/scooter-app/model/hardware/ \
+    bestscooter.azurecr.io/scooter-app
+
+    ((counter++))
 done
 
 # Denna funkar nog inte
